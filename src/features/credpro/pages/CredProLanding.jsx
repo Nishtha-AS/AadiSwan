94cb028fd449f2817a2cb23ce6628d6f5dcda636
Kiriti-aajad <kiriti.aajad@aadiswan.com>
2025-09-01 11:51:08 +0530

    credpro

---
+++ src/features/credpro/pages/CredProLanding.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CredProHeroSection from '../components/CredProHeroSection';
+import './CredProLanding.css';
+
+const CredProLanding = () => {
+  return (
+    <div className="credpro-landing">
+      <CredProHeroSection />
+    </div>
+  );
+};
+
+export default CredProLanding;
